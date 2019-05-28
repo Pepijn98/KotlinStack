3ac9afffbff9bc853c31f879ea72d48dfced758c
KurozeroPB <root@kurozeropb.info>
2019-05-28 20:19:56 +0200

    Check pubdate

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Twit from "twit";
 import settings from "../settings";
 import { FeedEmitter, FeedItem } from "rss-emitter-ts";
 
+const startTime = Date.now()
+
 const twitter = new Twit({
     consumer_key: settings.consumerKey,
     consumer_secret: settings.consumerSecret,
@@ -15,11 +17,16 @@ const rss = new FeedEmitter();
 rss.add({ url: settings.feed.url, ignoreFirst: true, refresh: settings.feed.refresh });
 
 rss.on("item:new", async (item: FeedItem) => {
-    try {
-        console.log(`Posting new question: ${item.title}`);
-        await twitter.post("statuses/update", { status: `${item.title}\n${item.link}` });
-        console.log("Send tweet!");
-    } catch (e) {
-        console.error(e);
+    if (item.pubdate) {
+        const time = Date.parse(item.pubdate.toISOString())
+        if (time > startTime) {
+            try {
+                console.log(`Posting new question: ${item.title}`);
+                await twitter.post("statuses/update", { status: `${item.title}\n${item.link}` });
+                console.log("Send tweet!");
+            } catch (e) {
+                console.error(e);
+            }
+        }
     }
 });
